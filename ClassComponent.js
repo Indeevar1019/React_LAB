a32ab5f29eea1cd84aa239f62b58cf27d2dcb28a
Indeevar1019 <95519680+Indeevar1019@users.noreply.github.com>
2023-12-04 17:30:01 +0530

    Create ClassComponent.js

---
+++ ClassComponent.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+class ClassComponent extends Component{
+    render(){
+        return(<div>
+            <h3>This is a class Component</h3>
+            <p>Hello Leo!</p>
+        </div>);
+    }
+
+}
+
+export default ClassComponent;
